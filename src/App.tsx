6559a6387651325070819de3c72d4495e9c40a74
octuplesakura <octuplesakura@gmail.com>
2019-09-19 17:07:16 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
+
+declare const loadlive2d: any
 
 const App: React.FC = () => {
+  
+  useEffect(() => {
+    loadlive2d("live2d", '/model/model.json')
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="waifu-tips"></div>
+      <canvas id="live2d" width="280" height="250"></canvas>
     </div>
-  );
+  )
+
 }
 
 export default App;
